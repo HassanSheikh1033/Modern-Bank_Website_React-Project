8bc4e6bc0eac5625bddb6649e91d9a5ea212049e
HassanSheikh1033 <153464118+HassanSheikh1033@users.noreply.github.com>
2024-05-09 01:54:46 +0500

    Initial commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,33 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet
+        eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      {/* gradient start */}
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      {/* gradient end */}
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      {/* gradient end */}
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      {/* gradient end */}
+    </div>
+
+  </section>
+);
+
+export default CardDeal;
